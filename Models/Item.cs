f258772ea317f9d3efccb4b0c3667ecfe3e8d6f3
sarah <28868817@students.lincoln.ac.uk>
2025-05-01 14:55:16 +0100

    Item.cs

--- Models/Item.cs
+++ Models/Item.cs
@@ -2,11 +2,25 @@ using DungeonExplorer.Interfaces;
 
 namespace DungeonExplorer.Models
 {
+    /// <summary>
+    /// Abstract base class for all in game items that can be collected and used
+    /// Implements ICollectible interface to enable standardized collection behaviour
+    /// </summary>
     public abstract class Item : ICollectible
     {
+        /// <summary>
+        /// Gets the display name of the item
+        /// </summary>
         public string Name { get; protected set; }
+
+        /// <summary>
+        /// Abstract method defining the item's effect when used
+        /// </summary>
         public abstract void Use(Player player);
         
+        /// <summary>
+        /// Implementation of ICollectible.Collect, handles adding the item to player's inventory
+        /// </summary>
         public void Collect(Player player)
         {
             player.Inventory.AddItem(this);
@@ -14,16 +28,28 @@ namespace DungeonExplorer.Models
         }
     }
 
+    /// <summary>
+    /// Represents a weapon item that increases player's damage when equipped
+    /// </summary>
     public class Weapon : Item
     {
+        /// <summary>
+        /// Gets the damage boost value this weapon provides when equipped
+        /// </summary>
         public int DamageBoost { get; }
 
+        /// <summary>
+        /// Initialises a new weapon with specified properties
+        /// </summary>
         public Weapon(string name, int boost)
         {
             Name = name;
             DamageBoost = boost;
         }
 
+        /// <summary>
+        /// Equips the weapon to the player, increasing their damage stat
+        /// </summary>
         public override void Use(Player player)
         {
             player.Damage += DamageBoost;
@@ -31,16 +57,28 @@ namespace DungeonExplorer.Models
         }
     }
 
+    /// <summary>
+    /// Represents a consumable health-restoring item
+    /// </summary>
     public class Potion : Item
     {
+        /// <summary>
+        /// Gets the amount of health this potion restores
+        /// </summary>
         public int HealAmount { get; }
 
+        /// <summary>
+        /// Initialises a new health potion with specified properties
+        /// </summary>
         public Potion(string name, int heal)
         {
             Name = name;
             HealAmount = heal;
         }
 
+        /// <summary>
+        /// Consumes the potion, restoring player's health without exceeding maximum (100 HP)
+        /// </summary>
         public override void Use(Player player)
         {
             player.Health = Math.Min(100, player.Health + HealAmount);
@@ -48,16 +86,28 @@ namespace DungeonExplorer.Models
         }
     }
 
+    /// <summary>
+    /// Represents a key item used to unlock special doors or containers
+    /// </summary>
     public class Key : Item
     {
+        /// <summary>
+        /// Gets the description of what this key unlocks
+        /// </summary>
         public string DoorDescription { get; }
 
+        /// <summary>
+        /// Initialises a new key item with specified properties
+        /// </summary>
         public Key(string name, string doorDescription)
         {
             Name = name;
             DoorDescription = doorDescription;
         }
 
+        /// <summary>
+        /// Displays key information when used
+        /// </summary>
         public override void Use(Player player)
         {
             Console.WriteLine($"This key unlocks {DoorDescription}");

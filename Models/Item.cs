497acfa97a93d296987f1c251579b36bd95c3913
sarah <28868817@students.lincoln.ac.uk>
2025-05-01 10:02:46 +0100

    Item.cs

--- Models/Item.cs
+++ Models/Item.cs
@@ -3,18 +3,16 @@ using DungeonExplorer.Interfaces;
 namespace DungeonExplorer.Models
 {
     ///<summary>
-    ///Base item class implementing ICollectible
+    ///Abstract base class for all in-game items
+    ///Implements ICollectible interface to enable item collection behavior
     ///</summary>
     public abstract class Item : ICollectible
     {
         public string Name { get; protected set; }
 
-        public abstract void Use(Player player);
+        public abstract void Use(Player player); //Abstract method defining how the item affects the player when used
 
-        ///<summary>
-        ///Interface implementation
-        ///</summary>
-        public void Collect(Player player)
+        public void Collect(Player player) //Implementation of ICollectible interface, handles what happens when a player collects this item.
         {
             player.Inventory.AddItem(this);
             Console.WriteLine($"{player.Name} collected {Name}!");
@@ -23,15 +21,15 @@ namespace DungeonExplorer.Models
 
     public class Weapon : Item
     {
-        public int DamageBoost { get; }
+        public int DamageBoost { get; } //The amount of damage boost this weapon provides
 
-        public Weapon(string name, int boost) 
+        public Weapon(string name, int boost) //Constructs a new weapon with specified properties
         {
             Name = name;
             DamageBoost = boost;
         }
 
-        public override void Use(Player player)
+        public override void Use(Player player) //Applies the weapon's damage boost when equipped by a player
         {
             player.Damage += DamageBoost;
             Console.WriteLine($"{player.Name} equipped {Name} (+{DamageBoost} damage)!");
@@ -40,15 +38,15 @@ namespace DungeonExplorer.Models
 
     public class Potion : Item
     {
-        public int HealAmount { get; }
+        public int HealAmount { get; } //The amount of health this potion restores
 
-        public Potion(string name, int heal)
+        public Potion(string name, int heal) // Constructs a new health potion with specified properties
         {
             Name = name;
             HealAmount = heal;
         }
 
-        public override void Use(Player player)
+        public override void Use(Player player) //Restores player's health when consumed, capping at maximum 100 HP
         {
             player.Health = Math.Min(100, player.Health + HealAmount);
             Console.WriteLine($"{player.Name} healed {HealAmount} HP!");
